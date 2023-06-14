82b243288d79b93984305e4c9973db373bbf24d1
MLEX0 <marklex093571@gmail.com>
2023-06-14 08:08:32 +0300

    1.0

--- controller/mark.controller.js
+++ controller/mark.controller.js
@@ -3,10 +3,33 @@ const db = require('../db')
 class MarkController {
     async getMarkList(res, req){
         const id = res.params.id
-        const marks = await db.query('SELECT * FROM \"StudentGrade\" \"SG\" LEFT JOIN \"Lesson\" \"L\" ON \"SG\".\"IDLesson\" = \"L\".\"ID\" '
-        + 'LEFT JOIN \"Student\" \"ST\" ON \"SG\".\"IDStudent\" = \"ST\".\"ID\" WHERE \"L\".\"ID\" = $1;', [id])
+        const marks = await db.query('SELECT * FROM "FUNC_MarkListByID"($1);', [id])
         req.json(marks.rows)
+    }
+    async getStudentAllMarkList(res, req){
+        const id = res.params.id
+        const marks = await db.query('SELECT * FROM \"FUNC_AllStudentGrades\"($1) ORDER BY \"LessonDate\" DESC;', [id])
+        req.json(marks.rows)
+    }
+    async getStudentListByLessonID(res, req){
+        const id = res.params.id
+        const students = await db.query('SELECT * FROM \"Student\" WHERE \"IDStudyGroup\" = ' + 
+        '(SELECT \"IDStudyGroup\" FROM \"Lesson\" WHERE \"ID\" = $1);', [id])
+        req.json(students.rows)
+    }
+    async addMark(req, res){
+        const {IDStudent, IDGrade, IDLesson} = req.body
+        const user = await db.query('INSERT INTO public.\"StudentGrade\"(\"IDStudent\", \"IDGrade\", \"IDLesson\") VALUES ($1, $2, $3) RETURNING *', [IDStudent, IDGrade, IDLesson])
+        res.json(user.rows[0])
+    }
+    async editMark(req, res){
+        const {ID, IDStudent, IDGrade, IDLesson} = req.body
+        const user = await db.query('UPDATE public.\"StudentGrade\" SET \"IDStudent\"=$1, \"IDGrade\"=$2, \"IDLesson\"=$3 WHERE \"ID\" = $4 RETURNING *', [IDStudent, IDGrade, IDLesson, ID])
+        res.json(user.rows[0])
     }
 }
 
 module.exports = new MarkController()
+
+//SELECT * FROM "Student" WHERE "IDStudyGroup" = (SELECT "IDStudyGroup" FROM "Lesson" WHERE "ID" = 7)
+//INSERT INTO public."StudentGrade"("IDStudent", "IDGrade", "IDLesson") VALUES ($1, $2, $3);
--- routes/mark.routes.js
+++ routes/mark.routes.js
@@ -4,6 +4,9 @@ const router = new Router()
 const markController = require('../controller/mark.controller')
 
 router.get('/lesson/:id', passport.authenticate('jwt', {session:false}), markController.getMarkList)
-
+router.get('/student/:id', passport.authenticate('jwt', {session:false}), markController.getStudentListByLessonID)
+router.get('/studentall/:id', passport.authenticate('jwt', {session:false}), markController.getStudentAllMarkList)
+router.post('/add/', passport.authenticate('jwt', {session:false}), markController.addMark)
+router.post('/edit/', passport.authenticate('jwt', {session:false}), markController.editMark)
 
 module.exports = router
